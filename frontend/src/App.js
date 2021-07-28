687307d463548312d3599391f9453d335c43536c
lgaeun <gaeungaeun4837@gmail.com>
2021-07-28 16:01:33 +0900

    📝 add App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import { Grommet } from "grommet";
+import CategoryPage from "./pages/Category";
+import MainPage from "./pages/Main";
+
+function App() {
+  return (
+    <Grommet className="App">
+      {/* <MainPage /> */}
+      <CategoryPage />
+    </Grommet>
+  );
+}
+
+export default App;
