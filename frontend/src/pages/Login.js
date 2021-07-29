ff5e82bfa1d5a5bc7f3571889b3531e3f2c64b2b
Joohyun Kim <corinthionia@gmail.com>
2021-07-29 13:04:41 +0900

    :sparkles: add login page
    add login page

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,13 @@
+const Login = () => (
+  <div>
+    <form>
+      <input type="text" placeholder="ID" required />
+      <input type="password" placeholder="PW" required />
+      <input type="submit" value="Log In" />
+    </form>
+    <div>
+      <button>Google 계정으로 로그인</button>
+      <button>회원 가입</button>
+    </div>
+  </div>
+);
