e448afd1cbea4cf0a15909437082db5d40a380e7
Joohyun Kim <corinthionia@gmail.com>
2021-07-29 20:22:30 +0900

    :wrench: add login component
    add login component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { Grommet } from "grommet";
 import CategoryPage from "./pages/Category";
 import MainPage from "./pages/Main";
+import LoginPage from "./pages/Login";
 
 function App() {
   return (
     <Grommet className="App">
       {/* <MainPage /> */}
-      <CategoryPage />
+      {/* <CategoryPage /> */}
+      <LoginPage />
     </Grommet>
   );
 }
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,11 @@
-const Login = () => (
-  <div>
-    <form>
-      <input type="text" placeholder="ID" required />
-      <input type="password" placeholder="PW" required />
-      <input type="submit" value="Log In" />
-    </form>
-    <div>
-      <button>Google 계정으로 로그인</button>
-      <button>회원 가입</button>
-    </div>
-  </div>
+import NavBar from "../components/NavBar/NavBar";
+import Login from "../components/Login/Login";
+
+const LoginPage = () => (
+  <>
+    <NavBar />
+    <Login />
+  </>
 );
+
+export default LoginPage;
