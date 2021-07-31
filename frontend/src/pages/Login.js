189f58e98bae8e1a2b08c27d0d20872a673492e9
Joohyun Kim <corinthionia@gmail.com>
2021-08-01 02:44:41 +0900

    :coffin: remove: Remove useless code
    remove: Remove useless code

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,11 @@
 import NavBar from "../components/NavBar/NavBar";
 import Login from "../components/Login/Login";
 import LoginTemplate from "../components/Login/LoginTemplate";
-import OtherLogin from "../components/Login/OtherLogin";
 
 const LoginPage = () => (
   <>
     <NavBar />
     <LoginTemplate form={<Login />} />
-    {/* <OtherLogin /> */}
   </>
 );
 
