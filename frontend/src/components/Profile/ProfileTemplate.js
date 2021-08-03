4b8723fdb01d9fb01dee0780a1e6cd0ea3a326e5
Joohyun Kim <corinthionia@gmail.com>
2021-08-03 19:44:44 +0900

    :sparkles: feat: Add Side bar component
    feat: Add Side bar component

--- frontend/src/components/Profile/ProfileTemplate.js
+++ frontend/src/components/Profile/ProfileTemplate.js
@@ -1,12 +1,19 @@
-import "../../style/mypage.css";
+import styled from "styled-components";
 
-const ProfileTemplate = ({ form, children }) => {
-  return (
-    <main className="login-template">
-      <section className="form-wrapper">{form}</section>
-      <section className="login-wrapper">{children}</section>
-    </main>
-  );
+const ProfileTemplateBlock = styled.div`
+  background: rgb(255, 255, 255);
+  box-shadow: 0 3px 6px rgba(87, 87, 87, 0.1), 0 3px 6px rgba(83, 83, 83, 0.23);
+
+  width: 1500px;
+  height: 1000px;
+  border-radius: 20px;
+
+  margin: 0 auto;
+  margin-top: 6rem;
+`;
+
+const ProfileTemplate = ({ children }) => {
+  return <ProfileTemplateBlock>{children}</ProfileTemplateBlock>;
 };
 
 export default ProfileTemplate;
--- frontend/src/pages/MyPage.js
+++ frontend/src/pages/MyPage.js
@@ -1,11 +1,14 @@
 import NavBar from "../components/NavBar/NavBar";
 import Profile from "../components/Profile/Profile";
 import ProfileTemplate from "../components/Profile/ProfileTemplate";
+import Sidebar from "../components/Profile/Sidebar";
 
 const MyPage = () => (
   <>
     <NavBar />
-    <ProfileTemplate form={<Profile />} />
+    <ProfileTemplate>
+      <Sidebar />
+    </ProfileTemplate>
   </>
 );
 
