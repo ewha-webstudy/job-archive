abd733b1dbff0daad0c384275ef8695d675178aa
Byun, Gyuri <byungyuri126@gmail.com>
2021-08-03 21:59:35 +0900

    :sparkles: add category compoenent, detail page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,32 @@
-import { Grommet } from "grommet";
+// import { Grommet } from "grommet";
+import { Route, Switch } from 'react-router-dom';
+
 import MainPage from "./pages/Main";
+import CategoryPage from "./pages/Category";
+import DetailInfoPage from './pages/DetailPage';
+import { Main } from 'grommet';
+
+// function App() {
+//   return (
+//       <Grommet className="App">
+//         <CategoryPage />
+//       </Grommet>
+//   );
+// }
 
 function App() {
   return (
-      <Grommet className="App">
-        <MainPage />
-      </Grommet>
+  <Switch>
+    <Route path='/' exact>
+      <MainPage />
+    </Route>
+    <Route path='/category'>
+      <CategoryPage />
+    </Route>
+    <Route path='/detail'>
+      <DetailInfoPage />
+    </Route>
+  </Switch>
   );
 }
 
