7c1d5ca8e7a7e632c9bb2c1dcb94ec2ab5802929
jina kong <kongjn226@gmail.com>
2021-08-10 19:00:27 +0900

    :hammer: update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,23 @@
-var express = require("express");
+"use strict";
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
 
-var app = express();
+const api = require('./api/members');
+const PORT = 3000;
 
+
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use("/", api);
+app.use('/api', api);
+
+
+app.listen(PORT, () =>{
+    console.log("서버 가동");
+}); 
+
+module.exports = app;
