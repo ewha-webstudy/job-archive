f343308e7cab2328d2ffb4b1d08fc031b99bef70
jina kong <kongjn226@gmail.com>
2021-08-10 18:47:36 +0900

    :hammer: update api/members/members.ctrl.js

---
+++ backend/api/members/members.ctrl.js
@@ -0,0 +1,36 @@
+"use strict";
+//const Member = require("../../models/membership"); 
+const Login = require("./login");
+const Signup = require("./signup");
+
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },    
+    login: (req, res) =>{
+        res.render("home/login");
+    },
+    signup: (req, res) =>{
+        res.render("home/signup");
+    }
+}
+
+const process ={
+    login: async (req, res) => {
+        const login = new Login(req.body);
+        const response = await login.login();
+        return res.json(response);
+    },
+    signup: async (req, res) => {
+        const signup = new Signup(req.body);
+        const response = await signup.signup();
+        return res.json(response);
+
+        
+    }
+};
+
+module.exports = {
+    output,
+    process
+};
