96dcd016de557ef5e004c9deaf493d9f491be716
hyosin Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-08-21 03:17:53 +0900

    fix: 해시라우터 삭제

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { HashRouter, Link } from "react-router-dom";
-import { Header, Button, Nav, Anchor, Grommet } from "grommet";
+import { Link } from "react-router-dom";
+import { Button, Nav, Anchor, Grommet } from "grommet";
 import "../../style/main.css";
 
 const Theme = {
@@ -15,43 +15,46 @@ const Theme = {
     hover: {
       textDecoration: "underline #FFA500",
       extend: "text-decoration-thickness 5px"
-    }
+    },
+    textDecoration: "none"
   }
 };
 
 class NavBar extends Component {
   render() {
     return (
-      <HashRouter>
-        <Grommet theme={Theme}>
-          <header className="menu">
-            <Nav direction="row">
-              <ul>
-                <Link to="/" className="home">
-                  JOB-ARCHIVE
-                </Link>
+      <Grommet theme={Theme}>
+        <header className="menu">
+          <Nav direction="row">
+            <ul>
+              <ul className="home">
+                <Link to="/">JOB-ARCHIVE</Link>
               </ul>
-              <li>
+            </ul>
+            <li>
+              <Link to="/category">
                 <Anchor color="black" label="프론트엔드" hover />
-              </li>
-              <li>
-                <Anchor color="black" label="백엔드" hoverIndicator />
-              </li>
-              <li>
-                <Anchor color="black" label="AI/데이터분석" hoverIndicator />
-              </li>
-              <li>
+              </Link>
+            </li>
+            <li>
+              <Anchor color="black" label="백엔드" hoverIndicator />
+            </li>
+            <li>
+              <Anchor color="black" label="AI/데이터분석" hoverIndicator />
+            </li>
+            <li>
+              <Link to="/login">
                 <Button
                   color="#ffaf00"
                   primary
                   label="Login"
                   alignSelf="center"
                 />
-              </li>
-            </Nav>
-          </header>
-        </Grommet>
-      </HashRouter>
+              </Link>
+            </li>
+          </Nav>
+        </header>
+      </Grommet>
     );
   }
 }
