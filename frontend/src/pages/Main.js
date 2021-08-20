923b524d15013ba4a7ef0dd263b8c78854cc52d2
hyosin Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-08-21 03:25:22 +0900

    chore: Maincard 컴포넌트 추가

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,22 +1,25 @@
 import React, { Component } from "react";
-import NavBar from "../components/NavBar/NavBar";
 import SearchBar from "../components/SearchBar/SearchBar";
-import Label from "../components/Label/Label";
+import { Link as ScrollLink } from "react-scroll";
+import Maincard from "./Maincard";
+
 import "../style/main.css";
 
 class Main extends Component {
   render() {
     return (
-      <div>
-        <header>
-          <NavBar />
-        </header>
-        <div className="container">
-          <h1>채용 정보 모음 서비스</h1>
-          <SearchBar />
-          <Label />
+      <>
+        <div className="main">
+          <div className="main-container">
+            <h1>채용 정보 모음 서비스</h1>
+            <SearchBar />
+            <br />
+            <h5>스크롤을 밑으로 내려 더 많은 정보를 확인해보세요!</h5>
+            {/* 화살표 */}
+          </div>
         </div>
-      </div>
+        <Maincard />
+      </>
     );
   }
 }
