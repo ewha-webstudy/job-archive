8e279e720bd30fad187b9cc99a521b53918cfd1e
hyosin Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-08-22 06:41:52 +0900

    chore: api 연결

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 import { Button, Nav, Anchor, Grommet } from "grommet";
 import "../../style/main.css";
 
@@ -23,38 +23,44 @@ const Theme = {
 class NavBar extends Component {
   render() {
     return (
-      <Grommet theme={Theme}>
-        <header className="menu">
-          <Nav direction="row">
-            <ul>
-              <ul className="home">
-                <Link to="/">JOB-ARCHIVE</Link>
+       <BrowserRouter>
+        <Grommet theme={Theme}>
+          <header className="menu">
+            <Nav direction="row">
+              <ul>
+                <ul className="home">
+                  <Link to="/">JOB-ARCHIVE</Link>
+                </ul>
               </ul>
-            </ul>
-            <li>
-              <Link to="/category">
-                <Anchor color="black" label="프론트엔드" hover />
-              </Link>
-            </li>
-            <li>
-              <Anchor color="black" label="백엔드" hoverIndicator />
-            </li>
-            <li>
-              <Anchor color="black" label="AI/데이터분석" hoverIndicator />
-            </li>
-            <li>
-              <Link to="/login">
-                <Button
-                  color="#ffaf00"
-                  primary
-                  label="Login"
-                  alignSelf="center"
-                />
-              </Link>
-            </li>
-          </Nav>
-        </header>
-      </Grommet>
+              <li>
+                <Link to="/api/category/frontend">
+                  <Anchor color="black" label="프론트엔드" hover />
+                </Link>
+              </li>
+              <li>
+                <Link to="/api/category/backend">
+                  <Anchor color="black" label="백엔드" hoverIndicator />
+                </Link>
+              </li>
+              <li>
+                <Link to="/api/category/data">
+                  <Anchor color="black" label="AI/데이터분석" hoverIndicator />
+                </Link>
+              </li>
+              <li>
+                <Link to="/login">
+                  <Button
+                    color="#ffaf00"
+                    primary
+                    label="Login"
+                    alignSelf="center"
+                  />
+                </Link>
+              </li>
+            </Nav>
+          </header>
+        </Grommet>
+      </BrowserRouter>
     );
   }
 }
