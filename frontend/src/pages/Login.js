244dabc06e8c5074b2b42e6d06b50e73beae5654
Joohyun Kim <corinthionia@gmail.com>
2021-08-26 23:11:06 +0900

    :goal_net: Catch errors in Login component
    Catch errors in Login component

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,24 +1,11 @@
 import NavBar from "../components/NavBar/NavBar";
-import Login from "../components/Login/Login";
 import LoginTemplate from "../components/Login/LoginTemplate";
-<<<<<<< HEAD
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-=======
 import LoginContainer from "../containers/LoginContainer";
->>>>>>> 27a44cc92aae759ce77b8de6c684e0072beb6de9
 const LoginPage = () => (
-  <Router>
+  <>
     <NavBar />
-<<<<<<< HEAD
-    <Route path="/login">
-      <LoginTemplate form={<Login />} />
-    </Route>
-  </Router>
-=======
     <LoginTemplate form={<LoginContainer />} />
   </>
->>>>>>> 27a44cc92aae759ce77b8de6c684e0072beb6de9
 );
 
 export default LoginPage;
