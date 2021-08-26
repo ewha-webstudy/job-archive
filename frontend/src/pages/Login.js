ec737da6dd118fa2bf277bb5d17d870ef1106722
hyosin Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-08-26 20:13:59 +0900

    feat: login container 경로 추가

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,7 @@
 import NavBar from "../components/NavBar/NavBar";
 import Login from "../components/Login/Login";
 import LoginTemplate from "../components/Login/LoginTemplate";
-
+import LoginContainer from "../containers/LoginContainer";
 const LoginPage = () => (
   <>
     <NavBar />
