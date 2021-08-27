bd52cbf21df8163ece0e88418db002a8e7af2e8f
hyosin Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-08-28 01:04:25 +0900

    feat: props 수정

--- frontend/src/containers/CardContainer.js
+++ frontend/src/containers/CardContainer.js
@@ -1,28 +1,13 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import Maincard from "../components/Card/Maincard";
-import { login, logout } from "../modules/logger";
-// 컨테이너는 페이지에 포함 시키기
 
 function CardContainer() {
   // store의 state 값 조회
-  const { islogin, token } = useSelector(state => ({
-    islogin: state.logger.islogin,
-    token: state.logger.token
+  const { islogin } = useSelector(state => ({
+    islogin: state.logger.islogin
   }));
 
-  const dispatch = useDispatch();
-
-  const onLogin = () => dispatch(login());
-  const onLogout = () => dispatch(logout());
-
-  return (
-    <Maincard
-      islogin={islogin}
-      token={token}
-      onLogin={onLogin}
-      onLogout={onLogout}
-    />
-  );
+  return <Maincard islogin={islogin} />;
 }
 export default CardContainer;
