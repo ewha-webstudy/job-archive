9d98859f2b9c70da7a7a8007a8e8832d00660b01
hyosin Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-08-29 03:58:19 +0900

    ✅ test: login token

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -11,25 +11,5 @@ const API = axios.create({
 
 API.defaults.headers.common['authorization'] = localStorage.getItem('token');
 
-// request interceptor
-API.interceptors.request.use(function(config){
-  config.headers.authorization = localStorage.getItem('token');
-  return config
-}, function(error){
-  console.error(error);
-  return Promise.reject(error)
-})
-
-// response interceptor
-API.interceptors.response.use(function(response){
-  console.log("interceptor response");
-  localStorage.setItem('token', response.data.token);
-
-
-}, function(error){
-  console.error(error);
-  return Promise.reject(error)
-})
-
 
 export default API;
