0deb323dcc444228b4215d99c55903aee6502659
hyosin Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-08-29 07:03:36 +0900

    ✨ feat: add animation

--- frontend/src/components/Card/JobCard.js
+++ frontend/src/components/Card/JobCard.js
@@ -1,23 +1,44 @@
 import { Card, CardHeader, Box, Image, Button } from "grommet";
 import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-// import allActions from "../../actions";
 import { Link } from "react-router-dom";
 import { Favorite } from "grommet-icons";
 import "../../style/card.css";
 import API from "../../utils/api";
 import GetDday from "./GetDday";
-
+import { motion } from "framer-motion";
+import { useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 function JobCard({ name, id, end, position, logo, likeNo, islogin }) {
 
   const [isliked, setLiked] = useState(false);
   const [numLikes, setnumLikes] = useState(likeNo);
 
+  const { ref, inView } = useInView({
+    threshold: 0.1
+  });
+  const animation = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      animation.start({
+        y: -100,
+        opacity: 1,
+        transition: { duration: 0.5, ease: "easeInOut" }
+      });
+    }
+    if (!inView) {
+      animation.start({
+        y: 0,
+        opacity: 0,
+        transition: { duration: 0.5, ease: "easeInOut" }
+      });
+    }
+    // console.log("print inView", inView);
+  }, [inView]);
+
   const sendnumLikes = () => {
-    // 좋아요를 누른거라면
     if (!isliked) {
-      // user 테이블 좋아요 추가
       console.log("isLiked status:", isliked);
       API.post(`/api/like`, { jobid: id })
         .then((response) => {
@@ -27,22 +48,17 @@ function JobCard({ name, id, end, position, logo, likeNo, islogin }) {
           console.error(error);
         });
 
-      // job 테이블 좋아요 추가
       API.post(`/api/likeIncrease`, { id: id })
         .then(response => {
           console.log(response);
-          // 좋아요 수를 res.likeNo로 설정
           setnumLikes(response.data.likeNo);
         })
         .catch(error => {
           console.error(error);
         });
 
-      // API.post("/api/likeIncrease")
     } else {
-      // 좋아요를 취소한거라면
       console.log("isLiked status:", isliked);
-      // user 테이블 좋아요 삭제
       API.delete(`/api/unlike/${id}`)
         .then((response) => {
           console.log(response);
@@ -50,12 +66,9 @@ function JobCard({ name, id, end, position, logo, likeNo, islogin }) {
         .catch((error) => {
           console.error(error);
         });
-
-      // job 테이블 좋아요 감소
       API.post(`/api/likeDecrease`, { id: id })
         .then(response => {
           console.log(response);
-          // 좋아요 수를 res.likeNo로 설정
           setnumLikes(response.data.likeNo);
         })
         .catch(error => {
@@ -65,32 +78,23 @@ function JobCard({ name, id, end, position, logo, likeNo, islogin }) {
   };
 
   const checkIsLogin = () => {
-    // 로그인되어 있고, liked가 false라면
     if (islogin && !isliked) {
-      // like 상태를 true로 바꾸고, numLike는 +1 해주기
       setLiked(!isliked);
       setnumLikes(numLikes);
-
-      // 서버로 좋아요 수 보낸다
       sendnumLikes();
     }
-
-    // 로그인되어 있고, liked가 true인 경우
     else if (islogin && isliked) {
       setLiked(!isliked);
       setnumLikes(numLikes);
-
       sendnumLikes();
     }
-
-    // 로그인 안되어 있는 경우
     else {
       alert("책갈피는 로그인 후 이용할 수 있습니다!");
     }
   };
 
-
   return (
+    <motion.div ref={ref} animate={animation} className="b-container">
     <Card width="small" background="light-1" responsive>
       <CardHeader height="xxsmall" pad="medium">
         <span className="card__name">{name}</span>
@@ -131,6 +135,7 @@ function JobCard({ name, id, end, position, logo, likeNo, islogin }) {
         </Link>
       </footer>
     </Card>
+    </motion.div>
   );
 }
 
