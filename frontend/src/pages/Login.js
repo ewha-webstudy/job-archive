cd62170bb0d93b79d123add776b11e973fc4bd4d
Joohyun Kim <corinthionia@gmail.com>
2021-08-31 02:51:00 +0900

    :coffin: Delete Fragment
    Delete Fragment

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,5 @@
 import LoginTemplate from "../components/Login/LoginTemplate";
 import LoginContainer from "../containers/LoginContainer";
-const LoginPage = () => (
-  <>
-    <LoginTemplate form={<LoginContainer />} />
-  </>
-);
+const LoginPage = () => <LoginTemplate form={<LoginContainer />} />;
 
 export default LoginPage;
