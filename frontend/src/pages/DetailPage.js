821827dbb9be15d631f7fe938b28972a7db496a8
haayun <kng06325@gmail.com>
2021-09-02 19:38:55 +0900

    update api url

--- frontend/src/pages/DetailPage.js
+++ frontend/src/pages/DetailPage.js
@@ -107,7 +107,7 @@ const DetailInfoPage = ({match}) => {
 			try {
 				setContent(null);
 				setError(null);
-				API.get(`api/category/${datakey}`).then((response) => {
+				API.get(`api/jobs/${datakey}`).then((response) => {
 					console.log(response.status);
 					setContent(response.data);
 				});
