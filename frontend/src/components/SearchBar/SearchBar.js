9facaf898564acc2f762ec73daeec7d3ecca410a
hyosin Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-09-06 01:01:26 +0900

    Update category routing path

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -2,62 +2,38 @@ import React, { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import "../../style/main.css";
-import API from "../../utils/api";
 
 function SearchBar(props) {
   const [searchTxt, setSearchTxt] = useState("");
 
-  const handleInputChange = e => {
-    setSearchTxt(e.target.value);
-  };
-
-  const data = {
-    tags: {
-      techStack: [],
-      enterTpCd: [],
-      avgSal: [],
-      region: [],
-      minEdubgIcd: []
-    },
-    searchBar: searchTxt
-  };
-
-  const onSubmit = async e => {
+  const handleBtnChecked = async e => {
     if (props.isBtnChecked === false) {
       alert("태그를 선택하세요");
       e.preventDefault();
-    } else {
-      await API.post(`/api/category/${props.btnValue}`, data)
-        .then(res => {
-          console.log(res.status);
-        })
-        .catch(error => {
-          if (error.response.status === 404) {
-            console.error(error);
-          }
-        });
     }
   };
 
   return (
-    <form action="" method="post">
-      <div className="searchbar__container">
-        <input
-          autoFocus
-          name="name"
-          type="text"
-          onChange={handleInputChange}
-          value={searchTxt}
-          className="searchbar__form"
-          placeholder="다양한 개발 직군과 채용정보를 검색해보세요!"
-        />
-        <Link to={`/category/${props.btnValue}/${searchTxt}`}>
-          <button className="searchbar__btn" type="submit" onClick={onSubmit}>
-            <BsSearch color="white" />
-          </button>
-        </Link>
-      </div>
-    </form>
+    <div className="searchbar__container">
+      <input
+        autoFocus
+        name="name"
+        type="text"
+        onChange={e => setSearchTxt(e.target.value)}
+        value={searchTxt}
+        className="searchbar__form"
+        placeholder="다양한 개발 직군과 채용정보를 검색해보세요!"
+      />
+      <Link to={`/category/${props.btnValue}/${searchTxt}`}>
+        <button
+          className="searchbar__btn"
+          type="submit"
+          onClick={handleBtnChecked}
+        >
+          <BsSearch color="white" />
+        </button>
+      </Link>
+    </div>
   );
 }
 
